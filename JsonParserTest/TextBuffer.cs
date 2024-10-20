9d738cd12f112f54dd9250fcf10c99638bad07d6
umai-beer <umai-beer@hotmail.co.jp>
2024-10-20 21:35:02 +0900

    ok

---
+++ JsonParserTest/TextBuffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JsonParserTest
+{
+    public class TextBuffer
+    {
+        char[] linefeed =[ '\n','\r' ];
+        public string Buffer { get; }
+
+        public int Length => Buffer.Length;
+
+        public int CurrentPosition { get; protected set; }
+
+        public int LinrCount {  get; protected set; }
+        public bool IsEmdOfBlock => CurrentPosition == Length;
+
+        public char Peek()
+        {
+            try
+            {
+                return Buffer[CurrentPosition];
+            }
+            catch (Exception)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void Next()
+        {
+            if (CurrentPosition == Length)
+            {
+                return;
+            }
+            CurrentPosition++;
+        }
+    }
+}
---
+++ JsonParserTest/TextVisitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonParserTest
+{
+    public class TextVisitor
+    {
+        protected TextBuffer _buffer;
+
+        public string Visit { get; protected set; } = string.Empty;
+
+        public char Current { get; protected set; }
+        public TextVisitor(TextBuffer buffer)
+        {
+            _buffer = buffer;
+        }
+
+        public void ClearVisit()=>Visit = string.Empty;
+
+        public bool Check(Func<char, bool> predicate)=>predicate(Current);
+
+        public void Add(string addBuffer) => Visit += addBuffer;
+
+
+    }
+}
